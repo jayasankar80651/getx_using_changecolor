864e10d61e22c5a2f5e9d8a1f1c5f6b55fb0c43b
jayasankar80651 <jaya80651@gmail.com>
2025-11-20 23:34:03 +0530

    second commite 20/11/25

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
 import 'package:getxpackage_changecolor/home.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: ColorChangeUsingGetx (),
-      debugShowCheckedModeBanner: false,
-    );
+  runApp(GetMaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: ColorChangeUsingGetx()
+  ));
   }
-}
 
      
